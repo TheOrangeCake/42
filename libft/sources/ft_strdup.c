a75d72776ed086a5a63fe6c62a1c38dcb547cd4f
Nguyen NGUYEN <154276946+TheOrangeCake@users.noreply.github.com>
2024-10-06 20:44:51 +0200

    the rest of the first mandatory part

---
+++ libft/sources/ft_strdup.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/04 19:44:26 by hoannguy          #+#    #+#             */
+/*   Updated: 2024/10/04 19:44:26 by hoannguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+static size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*ptr;
+	int		i;
+
+	i = 0;
+	ptr = malloc(ft_strlen(s) + 1);
+	if (ptr == NULL)
+		return (NULL);
+	while (s[i])
+	{
+		ptr[i] = s[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
