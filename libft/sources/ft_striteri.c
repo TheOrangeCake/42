305d0f7a6bf9f93ceb96848fb7019303bd6505c5
Nguyen NGUYEN <154276946+TheOrangeCake@users.noreply.github.com>
2024-10-09 13:52:26 +0200

    Create ft_striteri.c

---
+++ libft/sources/ft_striteri.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/09 13:42:21 by hoannguy          #+#    #+#             */
+/*   Updated: 2024/10/09 13:51:03 by hoannguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	int		i;
+
+	i = 0;
+	while (s[i])
+	{
+		(*f)(i, &s[i]);
+		i++;
+	}
+}
+
+// #include <stdio.h>
+
+// void my_func(unsigned int i, char *c) {
+//     *c = *c + i;
+// }
+
+// int main() {
+//     char str[] = "123";
+//     ft_striteri(str, my_func);
+//     printf("%s\n", str);
+//     return 0;
+// }
