044190ee88c6e2a4d90e36f9915a2aab8db8fc5a
Nguyen NGUYEN <154276946+TheOrangeCake@users.noreply.github.com>
2024-10-11 11:51:54 +0200

    Update ft_substr.c

--- libft/libft/ft_substr.c
+++ libft/libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 20:50:15 by hoannguy          #+#    #+#             */
-/*   Updated: 2024/10/09 16:26:11 by hoannguy         ###   ########.fr       */
+/*   Updated: 2024/10/11 11:51:46 by hoannguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,17 +27,16 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*ptr;
-	int		i;
+	size_t	i;
 	size_t	s_len;
 
 	i = 0;
-	start = start - 1;
 	s_len = ft_strlen(s);
-	if (start >= s_len)
-		return (NULL);
 	if (start + len > s_len)
 		len = s_len - start;
-	ptr = malloc(len + 1);
+	if (start >= s_len)
+		len = 0;
+	ptr = malloc(sizeof(char) * (len + 1));
 	if (ptr == NULL)
 		return (NULL);
 	while (len > 0)
@@ -54,12 +53,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 // int	main(void)
 // {
-// 	char	test[] = "1234567";
+// 	char	test[] = "FULL BULLSHIT";
 // 	int		start;
 // 	int		len;
 
-// 	start = 3;
-// 	len = 6;
+// 	start = 400;
+// 	len = 20;
 // 	printf("Result: %s\n", ft_substr(test, start, len));
 // 	return (0);
 // }
