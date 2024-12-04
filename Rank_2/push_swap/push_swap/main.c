52c86ec9b5250795dd1afc47b3f8b44ca9b6dbfc
TheOrangeCake <vietfox94@gmail.com>
2024-12-04 12:19:35 +0100

    update pushswap

--- Rank_2/push_swap/push_swap/main.c
+++ Rank_2/push_swap/push_swap/main.c
@@ -6,7 +6,33 @@
 /*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 10:59:53 by hoannguy          #+#    #+#             */
-/*   Updated: 2024/12/04 10:59:55 by hoannguy         ###   ########.fr       */
+/*   Updated: 2024/12/04 12:18:19 by hoannguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "pushswap.h"
+
+int	input_check(char *av[])
+{
+	return (0);
+}
+
+// void	push_swap(char *av[])
+// {
+	
+// }
+
+int	main(int ac, char *av[])
+{
+	if (ac > 0)
+	{
+		if (input_check(av) == 1)
+			return (write(2, "input error\n", 12), 0);
+		// push_swap(av);
+	}
+	else if (ac == 0)
+	{
+		ft_printf("placeholder");
+	}
+	return (0);
+}
--- Rank_2/push_swap/push_swap/pushswap.h
+++ Rank_2/push_swap/push_swap/pushswap.h
@@ -6,7 +6,20 @@
 /*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 11:00:10 by hoannguy          #+#    #+#             */
-/*   Updated: 2024/12/04 11:00:10 by hoannguy         ###   ########.fr       */
+/*   Updated: 2024/12/04 11:52:03 by hoannguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PUSHSWAP_H
+# define PUSHSWAP_H
+
+#include "libft.h"
+
+typedef struct s_pile
+{
+	int		number;
+	int		index;
+	void	*next;
+}	t_pile;
+
+#endif
