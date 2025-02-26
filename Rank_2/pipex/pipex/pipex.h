ae6e50b9b122a22193f94dc7113489c1b21530aa
root <root@Nguyen.>
2025-02-26 20:34:31 +0100

    pipex

--- Rank_2/pipex/pipex/pipex.h
+++ Rank_2/pipex/pipex/pipex.h
@@ -6,7 +6,7 @@
 /*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 17:56:33 by hoannguy          #+#    #+#             */
-/*   Updated: 2025/02/17 17:22:28 by hoannguy         ###   ########.fr       */
+/*   Updated: 2025/02/26 20:10:49 by hoannguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 # define PIPEX_H
 # include "libft.h"
 # include "stdio.h"
+
+typedef struct s_pipex
+{
+	int		fd_infile;
+	int		fd_outfile;
+	int		pipe[2];
+	pid_t	pid1;
+	pid_t	pid2;
+	char	*path_string;
+	char	**paths;
+} t_pipex;
 
 #endif
