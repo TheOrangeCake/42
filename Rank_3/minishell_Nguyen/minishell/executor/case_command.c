d8f8f44778e1667ce16e13af3cad7fe2a7d55fdd
TheOrangeCake <vietfox94@gmail.com>
2025-04-14 11:25:02 +0200

    strat executor

--- Rank_3/minishell_Nguyen/minishell/executor/case_command.c
+++ Rank_3/minishell_Nguyen/minishell/executor/case_command.c
@@ -1,20 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   case_command.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/24 15:15:04 by kcsajka           #+#    #+#             */
-/*   Updated: 2025/04/14 10:57:03 by hoannguy         ###   ########.fr       */
+/*   Created: 2025/04/14 11:17:33 by hoannguy          #+#    #+#             */
+/*   Updated: 2025/04/14 11:22:43 by hoannguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
-# include <stdio.h>
-# include <stdlib.h>
-# include <readline/readline.h>
-# include <readline/history.h>
+#include "executor.h"
 
-#endif
+void	case_cmd(t_node *tree, int *i)
+{
+	/* code to execute command
+	
+	
+	
+	*/
+}
--- Rank_3/minishell_Nguyen/minishell/executor/include/executor.h
+++ Rank_3/minishell_Nguyen/minishell/executor/include/executor.h
@@ -1,20 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   executor.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/24 15:15:04 by kcsajka           #+#    #+#             */
-/*   Updated: 2025/04/14 10:57:03 by hoannguy         ###   ########.fr       */
+/*   Created: 2025/04/14 10:50:58 by hoannguy          #+#    #+#             */
+/*   Updated: 2025/04/14 11:24:07 by hoannguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
-# include <stdio.h>
-# include <stdlib.h>
-# include <readline/readline.h>
-# include <readline/history.h>
+#ifndef EXECUTOR_H
+# define EXECUTOR_H
+# include "minishell.h"
+
+void	case_operator(t_node *tree, int *i);
+void	case_pipe(t_node *tree, int *i);
+void	case_redirection(t_node *tree, int *i);
+void	case_cmd(t_node *tree, int *i);
 
 #endif
