141be1d33da72a7f5a4dad3ce5506b294e96c7d2
TheOrangeCake <vietfox94@gmail.com>
2025-05-30 13:39:34 +0200

    minor

---
+++ Rank_4/cpp/cpp00/cpp00/ex02/Account.cpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/30 12:40:48 by hoannguy          #+#    #+#             */
+/*   Updated: 2025/05/30 13:12:15 by hoannguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+#include <iostream>
+
+// Account::_accountIndex = 0;
+// Account::_amount = 0;
+// Account::_nbDeposits = 0;
+// Account::_nbWithdrawals = 0;
