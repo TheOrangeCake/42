d6d38594517cd6fa60219cbfa7e60cc705985d2e
TheOrangeCake <vietfox94@gmail.com>
2025-07-25 16:35:00 +0200

    cpp07

--- Rank_5/cpp/cpp07/cpp07/ex01/iter.hpp
+++ Rank_5/cpp/cpp07/cpp07/ex01/iter.hpp
@@ -1,19 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   inter.hpp                                          :+:      :+:    :+:   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 11:38:48 by hoannguy          #+#    #+#             */
-/*   Updated: 2025/07/25 11:44:30 by hoannguy         ###   ########.fr       */
+/*   Updated: 2025/07/25 15:25:02 by hoannguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef INTER_HPP
 # define INTER_HPP
 
-template <typename U>
-void iter(U **a, )
+template <typename A, typename L>
+void iter(A *a, L len, void f(A)) {
+	int size = static_cast<int>(len);
+
+	for (int i = 0; i < size; i++) {
+		f(a[i]);
+	}
+}
 
 #endif
--- Rank_5/cpp/cpp07/cpp07/ex01/main.cpp
+++ Rank_5/cpp/cpp07/cpp07/ex01/main.cpp
@@ -6,8 +6,31 @@
 /*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 11:38:26 by hoannguy          #+#    #+#             */
-/*   Updated: 2025/07/25 11:38:44 by hoannguy         ###   ########.fr       */
+/*   Updated: 2025/07/25 15:29:51 by hoannguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "inter.hpp"
+#include "iter.hpp"
+#include <iostream>
+#include <string>
+
+template <typename T>
+void test_function1(T a) {
+	
+}
+
+void test_function(int i) {
+	std::cout << i << std::endl;
+}
+
+int main(void) {
+	{
+		int arr[5] = {1, 3, 5, 7 ,9};
+		size_t size = 5;
+		// int size = 5;
+		// float size = 5.9f;
+		// std::string size = "5";
+		
+		iter(arr, size, test_function);
+	}
+}
