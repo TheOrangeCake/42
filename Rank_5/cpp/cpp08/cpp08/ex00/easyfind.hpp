da4d61ec31a71309a16348188018bb5aef2023bb
TheOrangeCake <vietfox94@gmail.com>
2025-07-27 16:41:49 +0200

    start cpp08

---
+++ Rank_5/cpp/cpp08/cpp08/ex00/easyfind.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/27 16:22:11 by hoannguy          #+#    #+#             */
+/*   Updated: 2025/07/27 16:30:14 by hoannguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+#include <algorithm>
+
+template <typename T>
+void easyfind(T a, int b);
+
+#include "easyfind.tpp"
+#endif
---
+++ Rank_5/cpp/cpp08/cpp08/ex00/easyfind.tpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.tpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/27 16:22:41 by hoannguy          #+#    #+#             */
+/*   Updated: 2025/07/27 16:28:06 by hoannguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_TPP
+#define EASYFIND_TPP
+
+template <typename T>
+void easyfind(T a, int b) {
+	
+}
+
+#endif
